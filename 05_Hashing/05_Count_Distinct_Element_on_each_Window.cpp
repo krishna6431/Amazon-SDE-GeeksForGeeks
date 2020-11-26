1684431bd37785078cfb418b29ccc9e13b1c1b5d
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2020-11-26 22:38:52 +0530

    Added Problems on Hashing

---
+++ 05_Hashing/05_Count_Distinct_Element_on_each_Window.cpp
@@ -0,0 +1,64 @@
+//Amazon SDE GeeksForGeeks Problems
+//Code is Written by Krishna (krishna_6431)
+//Topic : Hashing
+
+// using sliding window technique;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> countDistinct(int[], int, int);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+        	cin >> a[i];
+        vector <int> result = countDistinct(a, n, k);
+        for (int i : result) 
+        	cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+
+vector <int> countDistinct (int arr[], int n, int k)
+{
+    //code here.
+    unordered_map<int,int>m;
+    vector<int>res;
+    int ans = 0;
+    for(int i = 0 ; i < k ; i++){
+        if(m[arr[i]]==0){
+            ans++;
+            m[arr[i]]++;
+        }
+        else{
+            m[arr[i]]++;
+            continue;
+        }
+        
+    }
+    res.push_back(ans);
+    for(int i = 1 ; i <= n-k ; i++){
+        if(m[arr[i-1]]==1){
+            ans--;
+        }
+        m[arr[i-1]] -= 1;
+        if(m[arr[i+k-1]]==0){
+            ans++;
+        }
+        m[arr[i+k-1]] += 1;
+        res.push_back(ans);
+    }
+    return res;
+}
