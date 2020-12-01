f34a44c0710a9d89773211a137ee76fe5a472aad
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2020-12-01 23:52:14 +0530

    Added Matrix Problems

---
+++ 04_Matrix/01_Rotate_By_90_degree.cpp
@@ -0,0 +1,60 @@
+//Amazon SDE GeeksForGeeks Problems
+//Code is Written by Krishna (krishna_6431)
+//Topic : Matrix
+
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    for(int i = 0; i < matrix.size(); i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+    for(int i = 0 ; i < matrix.size() ; i++){
+        for(int j = i ;j < matrix.size();j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        vector<vector<int> > matrix(n);
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
---
+++ 04_Matrix/02_Spiral_Matrix.cpp
@@ -0,0 +1,81 @@
+//Amazon SDE GeeksForGeeks Problems
+//Code is Written by Krishna (krishna_6431)
+//Topic : Matrix
+
+#include <iostream>
+using namespace std;
+
+#define SIZE 105
+
+void spirallyTraverse(int m, int n, int ar[SIZE][SIZE]);
+
+int main() {
+	int T = 0;
+	
+	scanf("%d",&T);
+	
+	while(T--)
+	{
+        int m,n;
+        scanf("%d",&m);
+        scanf("%d",&n);
+	    int ar[SIZE][SIZE] = {{0}};
+	    int i = 0;
+	    int j = 0;
+	    int row = 0;
+	    int col = 0;
+	    
+	    for(i=0; i<m; i++)
+	    {
+	        for(j=0; j<n; j++)
+	        {
+	            scanf("%d",&ar[i][j]);
+	        }
+	    }
+	    
+	    spirallyTraverse(m, n, ar);
+	    cout<<endl;
+	   
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+//Complete this function
+void spirallyTraverse(int n, int m, int arr[SIZE][SIZE]){
+    int sr = 0, er = n - 1, sc = 0, ec = m - 1;
+  int count = 0;
+  while (sr <= er && sc <= ec) {
+    for (int i = sc; i <= ec; i++) {
+      cout << arr[sr][i] << " ";
+      count++;
+    }
+    sr++;
+    if (count == n * m)
+      break;
+
+    for (int i = sr; i <= er; i++) {
+      cout << arr[i][ec] << " ";
+      count++;
+    }
+    ec--;
+    if (count == n * m)
+      break;
+
+    for (int i = ec; i >= sc; i--) {
+      cout << arr[er][i] << " ";
+      count++;
+    }
+    er--;
+    if (count == n * m)
+      break;
+
+    for (int i = er; i >= sr; i--) {
+      cout << arr[i][sc] << " ";
+      count++;
+    }
+    sc++;
+    if (count == n * m)
+      break;
+  }
+}
