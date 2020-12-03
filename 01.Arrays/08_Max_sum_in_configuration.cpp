d289e6185ec29afeb24bf7f821af270fa6854eca
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2020-12-03 23:47:48 +0530

    Update Max_Sum

--- 01.Arrays/08_Max_sum_in_configuration.cpp
+++ 01.Arrays/08_Max_sum_in_configuration.cpp
@@ -6,6 +6,7 @@
 /*
 Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation allowed is to rotate(clock-wise or counter clock-wise) the array any number of times.
 
+
 Example 1:
 
 Input:
@@ -34,50 +35,51 @@ Algorithm:
     3. Multipliers of other terms is decremented by 1. i.e., (cum_sum - arr[i-1]) is subtracted from current value where cum_sum is sum of all numbers
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int max_sum(int A[],int N);
+int max_sum(int A[], int N);
 
 int main()
 {
     int T;
-    cin>>T;
-    while(T--)
+    cin >> T;
+    while (T--)
     {
         int N;
-        cin>>N;
+        cin >> N;
         int A[N];
-        for(int i=0;i<N;i++)
+        for (int i = 0; i < N; i++)
         {
-            cin>>A[i];
+            cin >> A[i];
         }
 
-        cout<<max_sum(A,N)<<endl;
+        cout << max_sum(A, N) << endl;
         /*keeping track of the total sum of the array*/
-
     }
 }
 // } Driver Code Ends
 
-
 /*You are required to complete this method*/
-int max_sum(int arr[],int n)
-{   
+int max_sum(int arr[], int n)
+{
     // code is written by krishna
     int c_sum = 0;
-    for(int i =0 ; i < n ; i++){
-        c_sum+=arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        c_sum += arr[i];
     }
-    int c_val=0;
-    for(int i =0 ; i < n ; i++){
-        c_val += (arr[i]*i);
+    int c_val = 0;
+    for (int i = 0; i < n; i++)
+    {
+        c_val += (arr[i] * i);
     }
     int ans = c_val;
-    for(int i =1 ; i < n ; i++){
-        int n_val = c_val -(c_sum - arr[i-1]) + arr[i-1]*(n-1);
+    for (int i = 1; i < n; i++)
+    {
+        int n_val = c_val - (c_sum - arr[i - 1]) + arr[i - 1] * (n - 1);
         c_val = n_val;
-        ans = max(ans,n_val);
+        ans = max(ans, n_val);
     }
     return ans;
 }
