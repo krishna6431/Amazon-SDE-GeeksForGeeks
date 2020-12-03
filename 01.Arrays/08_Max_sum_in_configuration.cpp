c373f5de04b144df69a3b44070f2aafb02453091
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2020-12-04 00:15:04 +0530

    update max sum

--- 01.Arrays/08_Max_sum_in_configuration.cpp
+++ 01.Arrays/08_Max_sum_in_configuration.cpp
@@ -7,6 +7,7 @@
 Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation allowed is to rotate(clock-wise or counter clock-wise) the array any number of times.
 
 
+
 Example 1:
 
 Input:
