b339996475e3d89ba64d86d27885c461eee18e35
Clang Format <clang-format@1337z.ninja>
2020-12-07 17:45:26 +0000

    apply clang-format

--- 01.Arrays/10_Find_Duplicate_in_array.cpp
+++ 01.Arrays/10_Find_Duplicate_in_array.cpp
@@ -4,7 +4,8 @@
 
 // Problem Statement :
 /*
-Given an array a[] of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array.
+Given an array a[] of size N which contains elements from 0 to N-1, you need to find all the elements occurring more
+than once in the given array.
 
 Example 1:
 
@@ -23,15 +24,20 @@ array. Therefore output is -1.
 // Space Complexity : O(1);
 
 
-Approach: The basic idea is to use a HashMap to solve the problem. But there is a catch, the numbers in the array are from 0 to n-1, and the input array has length n. So, the input array can be used as a HashMap. While traversing the array, if an element a is encountered then increase the value of a%n'th element by n. The frequency can be retrieved by dividing the a%n'th element by n.
+Approach: The basic idea is to use a HashMap to solve the problem. But there is a catch, the numbers in the array are
+from 0 to n-1, and the input array has length n. So, the input array can be used as a HashMap. While traversing the
+array, if an element a is encountered then increase the value of a%n'th element by n. The frequency can be retrieved by
+dividing the a%n'th element by n.
 
-Algorithm:  
+Algorithm:
 
 1-->Traverse the given array from start to end.
 2-->For every element in the array increment the arr[i]%n'th element by n.
-3-->Now traverse the array again and print all those indices i for which arr[i]/n is greater than 1. Which guarantees that the number n has been added to that index.
+3-->Now traverse the array again and print all those indices i for which arr[i]/n is greater than 1. Which guarantees
+that the number n has been added to that index.
 
-Note: This approach works because all elements are in the range from 0 to n-1 and arr[i]/n would be greater than 1 only if a value "i" has appeared more than once. 
+Note: This approach works because all elements are in the range from 0 to n-1 and arr[i]/n would be greater than 1 only
+if a value "i" has appeared more than once.
 
 */
 
