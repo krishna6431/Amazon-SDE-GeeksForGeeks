31bf095cc0069ba1c5ec9f49dcf27b51e37080ad
Clang Format <clang-format@1337z.ninja>
2020-12-09 18:07:17 +0000

    apply clang-format

--- 01.Arrays/15_Product_Array_Puzzle.cpp
+++ 01.Arrays/15_Product_Array_Puzzle.cpp
@@ -4,9 +4,10 @@
 
 // Problem Statement :
 /*
-Given an array A[] of size N, construct a Product Array P (of same size N) such that P[i] is equal to the product of all the elements of A except A[i].
+Given an array A[] of size N, construct a Product Array P (of same size N) such that P[i] is equal to the product of all
+the elements of A except A[i].
 
- 
+
 
 Example 1:
 
@@ -14,12 +15,12 @@ Input:
 N = 5
 A[] = {10, 3, 5, 6, 2}
 Output: 180 600 360 300 900
-Explanation: 
+Explanation:
 For i=0, P[i] = 3*5*6*2 = 180.
 For i=1, P[i] = 10*5*6*2 = 600.
 For i=2, P[i] = 10*3*6*2 = 360.
 For i=3, P[i] = 10*3*5*2 = 300.
-For i=4, P[i] = 10*3*5*6 = 900. 
+For i=4, P[i] = 10*3*5*6 = 900.
 
 */
 
@@ -28,13 +29,14 @@ For i=4, P[i] = 10*3*5*6 = 900.
 // Space Complexity : O(n);
 
 
-Approach: 
+Approach:
 1. Compute product of all elements of array in prod(let).
-Now start traversing array print the quotient of prod dividing by current element i.e. arr[i]. Do this for remaining elements.
+Now start traversing array print the quotient of prod dividing by current element i.e. arr[i]. Do this for remaining
+elements.
 
 */
 
-//Initial template for C++
+// Initial template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -69,14 +71,14 @@ int main()
     return 0;
 } // } Driver Code Ends
 
-//User function template for C++
+// User function template for C++
 
 // nums: given vector
 // return the Product vector P that hold product except self at each index
 vector<long long int> productExceptSelf(vector<long long int> &nums, int n)
 {
 
-    //code here
+    // code here
     long long int m = 1;
     for (int i = 0; i < n; i++)
     {
--- 01.Arrays/16_Maximum_Index.cpp
+++ 01.Arrays/16_Maximum_Index.cpp
@@ -4,7 +4,8 @@
 
 // Problem Statement :
 /*
-Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] <= A[j].
+Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i]
+<= A[j].
 
 Example 1:
 
@@ -12,7 +13,7 @@ Input:
 N = 2
 A[] = {1,10}
 Output: 1
-Explanation: A[0]<=A[1] so (j-i) 
+Explanation: A[0]<=A[1] so (j-i)
 is 1-0 = 1.
 
 */
@@ -22,23 +23,25 @@ is 1-0 = 1.
 // Space Complexity : O(n);
 
 
-Approach: 
-As we need the max difference j - i such that A[i]<= A[j], hence we do not need to consider element after the index j and element before index i.
+Approach:
+As we need the max difference j - i such that A[i]<= A[j], hence we do not need to consider element after the index j
+and element before index i.
 
 But why..???
 
 Lets say, we get max difference for a particular i and j. Then these conditions hold true.
 
 A[i] <= A[j]
-Any element before A[i] is larger then A[j], else it would make the max difference. Hence we do not consider any element before A[i].
-Any element after A[j] is smaller then A[i], else it would make the max difference. Hence we do not consider any element after A[j].
-Now on basis of this analysis, how can we solve the question..?
+Any element before A[i] is larger then A[j], else it would make the max difference. Hence we do not consider any element
+before A[i]. Any element after A[j] is smaller then A[i], else it would make the max difference. Hence we do not
+consider any element after A[j]. Now on basis of this analysis, how can we solve the question..?
 
-For the observation, in previous hint, make 2 arrays, 
+For the observation, in previous hint, make 2 arrays,
 First, will store smallest occuring element before the element
 Second, will store largest occuring element after the element
 
-Traverse the Second array, till the element in second array is larger than or equal to First array, and store the index difference. And if it becomes smaller, traverse the first array till it again becomes larger.
+Traverse the Second array, till the element in second array is larger than or equal to First array, and store the index
+difference. And if it becomes smaller, traverse the first array till it again becomes larger.
 
 And store the max difference of this index difference.
 
@@ -87,27 +90,27 @@ int maxIndexDiff(int arr[], int n)
 int main()
 {
     int T;
-    //testcases
+    // testcases
     cin >> T;
     while (T--)
     {
         int num;
-        //size of array
+        // size of array
         cin >> num;
         int arr[num];
 
-        //inserting elements
+        // inserting elements
         for (int i = 0; i < num; i++)
             cin >> arr[i];
 
-        //printing 0 if size is 1
+        // printing 0 if size is 1
         if (num == 1)
         {
             cout << 0 << endl;
             continue;
         }
 
-        //calling maxIndexDiff() function
+        // calling maxIndexDiff() function
         cout << maxIndexDiff(arr, num) << endl;
     }
     return 0;
