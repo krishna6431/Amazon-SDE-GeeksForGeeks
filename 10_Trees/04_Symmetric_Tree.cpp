2bf9a2becec8510858be96303710de6fa9540f78
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-05-24 01:58:32 +0530

    added symmetric tree

---
+++ 10_Trees/04_Symmetric_Tree.cpp
@@ -0,0 +1,171 @@
+// Amazon SDE GeeksForGeeks Problems
+// Code is Written by Krishna (krishna_6431)
+// Topic : Trees
+
+// Problem Statement :
+/*
+Given a Binary Tree. Check whether it is Symmetric or not, i.e. whether the binary tree is a Mirror image of itself or not.
+
+Example 1:
+
+Input:
+         5
+       /   \
+      1     1
+     /       \
+    2         2
+Output: True
+Explanation: Tree is mirror image of
+itself i.e. tree is symmetric
+Example 2:
+
+Input:
+         5
+       /   \
+      10     10
+     /  \     \
+    20  20     30
+Output: False
+
+
+// Approach :
+// Time Complexity : O(N);
+// Space Complexity : O(height of tree);
+
+/*
+
+The idea is to write a recursive function isMirror() that takes two trees as an argument and returns true if trees are the mirror and false if trees are not mirrored. The isMirror() function recursively checks two roots and subtrees under the root.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+bool isSymmetric(struct Node *root);
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        Node *root = buildTree(s);
+        if (isSymmetric(root))
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+
+/*
+Structure of the node of the tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+//check function
+bool check(struct Node *root1, struct Node *root2)
+{
+    if (root1 == NULL && root2 == NULL)
+        return true;
+    if (root1 && root2 && root1->data == root2->data)
+    {
+        return check(root1->left, root2->right) && check(root1->right, root2->left);
+    }
+    return false;
+}
+// return true/false denoting whether the tree is Symmetric or not
+bool isSymmetric(struct Node *root)
+{
+    // Code here
+    return check(root, root);
+}
+
+//Thank U So Much
