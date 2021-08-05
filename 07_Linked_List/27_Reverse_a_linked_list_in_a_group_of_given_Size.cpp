b9cc9bc130993f2cc8b9926c837ad2f559fdecd3
Clang Format <clang-format@1337z.ninja>
2021-08-05 22:41:36 +0000

    apply clang-format

--- 07_Linked_List/27_Reverse_a_linked_list_in_a_group_of_given_Size.cpp
+++ 07_Linked_List/27_Reverse_a_linked_list_in_a_group_of_given_Size.cpp
@@ -4,17 +4,18 @@
 
 // Problem Statement :
 /*
-Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list.
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked
+list.
 
 Example 1:
 
 Input:
 LinkedList: 1->2->2->4->5->6->7->8
 K = 4
-Output: 4 2 2 1 8 7 6 5 
-Explanation: 
-The first 4 elements 1,2,2,4 are reversed first 
-and then the next 4 elements 5,6,7,8. Hence, the 
+Output: 4 2 2 1 8 7 6 5
+Explanation:
+The first 4 elements 1,2,2,4 are reversed first
+and then the next 4 elements 5,6,7,8. Hence, the
 resultant linked list is 4->2->2->1->8->7->6->5.
 
 */
@@ -25,9 +26,10 @@ resultant linked list is 4->2->2->1->8->7->6->5.
 
 /*
 
-Algorithm: reverse(head, k) 
+Algorithm: reverse(head, k)
 
-1->Reverse the first sub-list of size k. While reversing keep track of the next node and previous node. Let the pointer to the next node be next and pointer to the previous node be prev. See this post for reversing a linked list.
+1->Reverse the first sub-list of size k. While reversing keep track of the next node and previous node. Let the pointer
+to the next node be next and pointer to the previous node be prev. See this post for reversing a linked list.
 2->head->next = reverse(next, k) ( Recursively call for rest of the list and link the two sub-lists )
 3->Return prev ( prev becomes the new head of the list (see the diagrams of an iterative method of this post) )
 
@@ -61,28 +63,28 @@ void printList(struct node *node)
 
 /*
   Reverse a linked list
-  The input list will have at least one element  
+  The input list will have at least one element
   Return the node which points to the head of the new LinkedList
-  Node is defined as 
+  Node is defined as
     struct node
     {
         int data;
         struct node* next;
-    
+
         node(int x){
             data = x;
             next = NULL;
         }
-    
+
     }*head;
 */
 
 class Solution
 {
-public:
+  public:
     struct node *reverse(struct node *head, int k)
     {
-        //base case
+        // base case
         if (head == NULL)
         {
             return NULL;
@@ -104,7 +106,7 @@ public:
 
         if (temp != NULL)
         {
-            //trickier case think using pen paper
+            // trickier case think using pen paper
             head->next = reverse(temp, k);
         }
         return prev;
