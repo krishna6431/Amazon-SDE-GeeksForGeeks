1e4f0646dc92692a22faf363257c9246ae0eb9e7
Clang Format <clang-format@1337z.ninja>
2021-08-17 10:10:11 +0000

    apply clang-format

--- 08_Stack/03_Implement_two_stack_in_array.cpp
+++ 08_Stack/03_Implement_two_stack_in_array.cpp
@@ -6,7 +6,7 @@
 /*
 Your task is to implement  2 stacks in one array efficiently.
 
- 
+
 
 Example 1:
 
@@ -25,9 +25,9 @@ Explanation:
 push1(2) the stack1 will be {2}
 push1(3) the stack1 will be {2,3}
 push2(4) the stack2 will be {4}
-pop1()   the poped element will be 3 
+pop1()   the poped element will be 3
 from stack1 and stack1 will be {2}
-pop2()   the poped element will be 4 
+pop2()   the poped element will be 4
 from stack2 and now stack2 is empty
 pop2()   the stack2 is now empty hence -1 .
 
@@ -36,25 +36,34 @@ pop2()   the stack2 is now empty hence -1 .
 // Expected Time Complexity: O(1) for all the four methods.
 // Expected Auxiliary Space: O(1) for all the four methods.
 /*
-Create a data structure twoStacks that represents two stacks. Implementation of twoStacks should use only one array, i.e., both stacks should use the same array for storing elements. Following functions must be supported by twoStacks.
-push1(int x) --> pushes x to first stack 
+Create a data structure twoStacks that represents two stacks. Implementation of twoStacks should use only one array,
+i.e., both stacks should use the same array for storing elements. Following functions must be supported by twoStacks.
+push1(int x) --> pushes x to first stack
 push2(int x) --> pushes x to second stack
-pop1() --> pops an element from first stack and return the popped element 
+pop1() --> pops an element from first stack and return the popped element
 pop2() --> pops an element from second stack and return the popped element
 Implementation of twoStack should be space efficient.
- 
 
 
 
 
-Method 1 (Divide the space in two halves) 
-A simple way to implement two stacks is to divide the array in two halves and assign the half half space to two stacks, i.e., use arr[0] to arr[n/2] for stack1, and arr[(n/2) + 1] to arr[n-1] for stack2 where arr[] is the array to be used to implement two stacks and size of array be n. 
-The problem with this method is inefficient use of array space. A stack push operation may result in stack overflow even if there is space available in arr[]. For example, say the array size is 6 and we push 3 elements to stack1 and do not push anything to second stack2. When we push 4th element to stack1, there will be overflow even if we have space for 3 more elements in array.
+
+Method 1 (Divide the space in two halves)
+A simple way to implement two stacks is to divide the array in two halves and assign the half half space to two stacks,
+i.e., use arr[0] to arr[n/2] for stack1, and arr[(n/2) + 1] to arr[n-1] for stack2 where arr[] is the array to be used
+to implement two stacks and size of array be n. The problem with this method is inefficient use of array space. A stack
+push operation may result in stack overflow even if there is space available in arr[]. For example, say the array size
+is 6 and we push 3 elements to stack1 and do not push anything to second stack2. When we push 4th element to stack1,
+there will be overflow even if we have space for 3 more elements in array.
 
 
 
-Method 2 (A space efficient implementation) 
-This method efficiently utilizes the available space. It doesn't cause an overflow if there is space available in arr[]. The idea is to start two stacks from two extreme corners of arr[]. stack1 starts from the leftmost element, the first element in stack1 is pushed at index 0. The stack2 starts from the rightmost corner, the first element in stack2 is pushed at index (n-1). Both stacks grow (or shrink) in opposite direction. To check for overflow, all we need to check is for space between top elements of both stacks. This check is highlighted in the below code. 
+Method 2 (A space efficient implementation)
+This method efficiently utilizes the available space. It doesn't cause an overflow if there is space available in arr[].
+The idea is to start two stacks from two extreme corners of arr[]. stack1 starts from the leftmost element, the first
+element in stack1 is pushed at index 0. The stack2 starts from the rightmost corner, the first element in stack2 is
+pushed at index (n-1). Both stacks grow (or shrink) in opposite direction. To check for overflow, all we need to check
+is for space between top elements of both stacks. This check is highlighted in the below code.
 
 */
 
@@ -68,7 +77,7 @@ class twoStacks
     int size;
     int top1, top2;
 
-public:
+  public:
     twoStacks(int n = 100)
     {
         size = n;
@@ -122,7 +131,7 @@ int main()
     }
 }
 
-//Function to push an integer into the stack1.
+// Function to push an integer into the stack1.
 void twoStacks ::push1(int x)
 {
 
@@ -130,14 +139,14 @@ void twoStacks ::push1(int x)
     arr[top1] = x;
 }
 
-//Function to push an integer into the stack2.
+// Function to push an integer into the stack2.
 void twoStacks ::push2(int x)
 {
     top2--;
     arr[top2] = x;
 }
 
-//Function to remove an element from top of the stack1.
+// Function to remove an element from top of the stack1.
 int twoStacks ::pop1()
 {
     if (top1 == -1)
@@ -149,7 +158,7 @@ int twoStacks ::pop1()
     return x;
 }
 
-//Function to remove an element from top of the stack2.
+// Function to remove an element from top of the stack2.
 int twoStacks ::pop2()
 {
     if (top2 == size)
