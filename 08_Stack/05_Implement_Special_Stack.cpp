094d472fbf15666bfb3c7e4b80441822ebaae51c
Krishna Kant Verma <55027576+krishna6431@users.noreply.github.com>
2021-08-20 17:16:06 -0400

    Create 05_Implement_Special_Stack.cpp

---
+++ 08_Stack/05_Implement_Special_Stack.cpp
@@ -0,0 +1,88 @@
+// Amazon SDE GeeksForGeeks Problems
+// Code is Written by Krishna (krishna_6431)
+// Topic : Stacks
+
+// Problem Statement :
+/*
+Design a data-structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull() and an additional operation getMin() which should return minimum element from the SpecialStack. Your task is to complete all the functions, using stack data-Structure.
+
+
+Example 1:
+
+Input:
+Stack: 18 19 29 15 16
+Output: 15
+Explanation:
+The minimum element of the stack is 15.
+
+// Approach :
+// Expected Time Complexity: O(N) for getMin, O(1) for remaining all 4 functions.
+// Expected Auxiliary Space: O(1) for all the 5 functions.
+
+
+/*
+
+
+*/
+
+#include<iostream>
+#include<stack>
+using namespace std;
+void push(stack<int>& s,int a);
+bool isFull(stack<int>& s,int n);
+bool isEmpty(stack<int>& s);
+int pop(stack<int>& s);
+int getMin(stack<int>& s);
+//This is the STL stack (http://quiz.geeksforgeeks.org/stack-container-adaptors-the-c-standard-template-library-stl/).
+stack<int> s;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a;
+		cin>>n;
+		while(!isEmpty(s)){
+		    pop(s);
+		}
+		while(!isFull(s,n)){
+			cin>>a;
+			push(s,a);
+		}
+		cout<<getMin(s)<<endl;
+	}
+}
+
+
+void push(stack<int>& s, int a){
+	// Your code goes here
+	s.push(a);
+}
+
+bool isFull(stack<int>& s,int n){
+	// Your code goes here
+	return s.size()==n;
+}
+
+bool isEmpty(stack<int>& s){
+	// Your code goes here
+	return s.empty();
+}
+
+int pop(stack<int>& s){
+	// Your code goes here
+	int x = s.top();
+	s.pop();
+	return x;
+}
+
+int getMin(stack<int>& s){
+	// Your code goes here
+	int m = s.top();
+	while(!s.empty()){
+	    m= min(m,s.top());
+	    s.pop();
+	}
+	return m;
+}
+
+// Thank U So Much
